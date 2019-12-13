30fae80d4b6d13b5b7f096201de478fe33fba6c6
Haraelai <58796465+Haraelai@users.noreply.github.com>
2019-12-13 00:10:28 -0600

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Science Technology Engineerig & Mathematics</title>
-    <link rel="stylesheet" href="">
     <link rel="stylesheet" href="CSS/estilos.css">
     <link rel="stylesheet" href="CSS/problema.css"/>
     <link href="https://fonts.googleapis.com/css?family=Rambla" rel="stylesheet">
